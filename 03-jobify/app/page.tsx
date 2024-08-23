918202f5a1d04052925e6055becf0d686c018594
Oleksandr Morozov <mopoz07@gmail.com>
2024-08-23 11:49:51 +0300

    Update tailwind typo error in page.tsx
    Update tailwind typo error in page.tsx

--- 03-jobify/app/page.tsx
+++ 03-jobify/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
         <Image src={Logo} alt='logo' />
       </header>
-      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
+      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
         <div>
           <h1 className='capitalize text-4xl md:text-7xl font-bold'>
             job <span className='text-primary'>tracking</span> app
